007722b9bb9c4c963fbe33e93f5331dec42f31cc
metacubed <7028125+metacubed@users.noreply.github.com>
2026-06-30 19:38:29 +0530

    Filter for player activation of owned objects

--- scripts/block_stealing/global.lua
+++ scripts/block_stealing/global.lua
@@ -3,8 +3,13 @@ local types = require('openmw.types')
 local acti = require('openmw.interfaces').Activation
 
 local function onActivate(obj, actor)
+    if (actor.type ~= types.Player) then return end
+
+    if (not obj.owner.recordId) then return end
+    if (obj.owner.recordId == actor.recordId) then return end
+
     actor:sendEvent('ShowMessage', {
-        message = 'Blocked activation of ' .. obj.recordId .. ' owned by ' .. (obj.owner.recordId or 'no one')
+        message = 'Blocked activation of ' .. obj.recordId .. ' owned by ' .. obj.owner.recordId
     })
     return false
 end
