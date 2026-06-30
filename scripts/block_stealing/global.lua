322757b71f8d876479c3584a621cac14debafa69
metacubed <7028125+metacubed@users.noreply.github.com>
2026-06-30 11:08:16 +0530

    Handle activation for items, containers and doors

--- scripts/block_stealing/global.lua
+++ scripts/block_stealing/global.lua
@@ -1,3 +1,17 @@
 local core = require('openmw.core')
+local types = require('openmw.types')
+local acti = require('openmw.interfaces').Activation
+
+local function onActivate(obj, actor)
+    actor:sendEvent('ShowMessage', {
+        message = 'Activated ' .. obj.recordId .. ' owned by ' .. (obj.owner.recordId or 'no one')
+    })
+end
+
+for _, type in pairs(types) do
+    if (type.baseType == types.Item or type.baseType == types.Lockable) then
+        acti.addHandlerForType(type, onActivate)
+    end
+end
 
 return {}
